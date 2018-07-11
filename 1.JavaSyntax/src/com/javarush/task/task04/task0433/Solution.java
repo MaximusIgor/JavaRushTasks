c834212454fb368d76f7885abc6e39c32478fa16
Игорь <shilov0209@gmail.com>
2018-07-12 00:55:57 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0433/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task04.task0433;
+
+
+/* 
+Гадание на долларовый счет
+*/
+
+public class Solution {
+    public static void main(String[] args)  {
+        //напишите тут ваш код
+
+        int money = 0;
+
+        while (money < 10) {
+            System.out.println("SSSSSSSSSS");
+            money++;
+        }
+    }
+}
