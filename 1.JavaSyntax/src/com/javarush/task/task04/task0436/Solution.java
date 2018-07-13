3f61864dc4f251ad27704a0a33d23bd2837f5427
Игорь <shilov0209@gmail.com>
2018-07-13 04:49:45 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0436/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task04.task0436;
+
+
+/* 
+Рисуем прямоугольник
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int a = 0; a != m; a++) {
+
+            for (int i = 0; i != n; i++) {
+
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+    }
+}
