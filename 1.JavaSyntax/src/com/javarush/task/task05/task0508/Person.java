c9e2533e892f9fbdaad47af3559b9ae4fc6e286b
Игорь <shilov0209@gmail.com>
2018-07-17 01:58:24 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0508/Person.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task05.task0508;
+
+/* 
+Геттеры и сеттеры для класса Person
+*/
+
+public class Person {
+    //напишите тут ваш код
+    String name;
+    int age;
+    char sex;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public char getSex(){
+        return sex;
+    }
+
+  public void setName (String newName){
+        this.name = newName;
+  }
+
+  public void setAge(int newAge){
+        this.age = newAge;
+  }
+
+  public void setSex (char newSex){
+        this.sex = newSex;
+  }
+
+    public static void main(String[] args) {
+
+    }
+}
