0997e208273f67317f574d11ae03bfdcd82b58c1
Игорь <shilov0209@gmail.com>
2018-07-30 22:48:21 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0612/Test.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task06.task0612;
+
+public class Test {
+    public static void main(String args[]){
+        StringBuffer sBuffer = new StringBuffer("тест");
+        sBuffer.append(" StringBuffer");
+        System.out.println(sBuffer);
+    }
+}
