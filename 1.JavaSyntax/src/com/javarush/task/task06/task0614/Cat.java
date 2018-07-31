48f64cf501a0b669ed19076d18974e6f4ffe45ca
Игорь <shilov0209@gmail.com>
2018-08-01 00:42:31 +0300

    Solved.

--- 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
@@ -8,17 +8,23 @@ import java.util.ArrayList;
 
 public class Cat {
     //напишите тут ваш код
-    public static ArrayList cats = new ArrayList();
+    private static ArrayList<Cat> cats = new ArrayList<Cat>();
 
     public Cat() {
     }
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        for (int i = 0; i < 10; i++){
+            cats.add(new Cat());
+        }
         printCats();
     }
 
-    public static void printCats() {
+    private static void printCats() {
         //напишите тут ваш код
+        for (int i = 0; i < 10; i++){
+            System.out.println(cats.get(i));
+        }
     }
 }
