0a4f3ef17910b95d8033ac78fd0934f8f1c94eff
Игорь <shilov0209@gmail.com>
2018-08-02 20:09:48 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Test.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task06.task0622;
+
+public class Test {
+    public static void main(String[] args) {
+        String [] list = new String [5];
+
+        for (int i = 0; i < list.length ; i++) {
+            System.out.println(list[i]);
+        }
+    }
+}
