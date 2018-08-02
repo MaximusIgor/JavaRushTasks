2c6378dd26e8ee8d52e54160106cdb4967dc8a4a
Игорь <shilov0209@gmail.com>
2018-08-03 01:20:50 +0300

    Solved.

--- 1.JavaSyntax/src/com/javarush/task/task06/task0616/Test.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0616/Test.java
@@ -1,4 +1,17 @@
 package com.javarush.task.task06.task0616;
 
+/*
+Быстрая (статическая) инициализация. Сумма элементов массива
+ */
+
 public class Test {
+    public static void main(String[] args) {
+        int[] list = {5, 6, 7, 8, 1, 2, 5, -7, -9, 2, 0};
+
+        int sum = 0;
+        for (int i = 0; i < list.length; i++) {
+            sum += list[i];
+        }
+        System.out.println("Sum is " + sum);
+    }
 }
