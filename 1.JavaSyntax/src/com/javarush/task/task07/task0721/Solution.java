70bb7402bdc2e845220a61830688c845812ea946
Игорь <shilov0209@gmail.com>
2018-08-21 01:19:43 +0300

    Solved.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0721/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0721/Solution.java
@@ -23,7 +23,7 @@ public class Solution {
         }
         maximum = arr[0];
         minimum = arr[0];
-        
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= maximum) {
                 maximum = arr[i];
