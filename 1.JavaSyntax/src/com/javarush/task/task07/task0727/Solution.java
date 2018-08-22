24c10dbb026345fc901eab24cc160a87238d0c80
Игорь <shilov0209@gmail.com>
2018-08-22 19:58:56 +0300

    Solved.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0727/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0727/Solution.java
@@ -14,20 +14,35 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> newList = new ArrayList<String>();
         while (true) {
             String s = reader.readLine();
-            if (s.isEmpty()) break;
+
+            if (s.isEmpty())
+                break;
             list.add(s);
         }
 
-        ArrayList<String> listUpperCase = new ArrayList<String>();
+//        ArrayList<String> listUpperCase = new ArrayList<String>();
+//        for (int i = 0; i < list.size(); i++) {
+//            String s = list.get(i);
+//            listUpperCase.add(s.toUpperCase());
+//        }
+//
+//        for (int i = 0; i < listUpperCase.size(); i++) {
+//            System.out.println(listUpperCase.get(i));
+//        }
+
         for (int i = 0; i < list.size(); i++) {
             String s = list.get(i);
-            listUpperCase.add(s.toUpperCase());
+            if (s.length() % 2 != 0)
+                newList.add(s + " " + s + " " + s);
+            else
+                newList.add(s + " " + s);
         }
 
-        for (int i = 0; i < listUpperCase.size(); i++) {
-            System.out.println(listUpperCase.get(i));
+        for (String s: newList) {
+            System.out.println(s);
         }
     }
 }
