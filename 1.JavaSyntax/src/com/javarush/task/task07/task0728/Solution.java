5d78453447023e3b219f96b5b21ee59f263df73d
Игорь <shilov0209@gmail.com>
2018-08-24 17:44:52 +0300

    Solved.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -24,8 +24,15 @@ public class Solution {
 
     public static void sort(int[] array) {
         //напишите тут ваш код
-        for (int i = 0; i < array.length ; i++) {
-
+        int tmp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
         }
     }
 }
