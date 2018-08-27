5db553684d0bed2e47b8738dcaf39ca9305ed53c
Игорь <shilov0209@gmail.com>
2018-08-27 23:17:22 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0727/Test.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task07.task0727;
+
+public class Test {
+    static void sayHello(String name){
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        sayHello("Java");
+    }
+}
