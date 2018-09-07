92a0c6214ae523181050192dc3793b0b8010dcde
Игорь <shilov0209@gmail.com>
2018-09-07 20:28:26 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/ListElements.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task07.task0728;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListElements {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("Mama");
+        list.add("Mila");
+        list.add("Ramu");
+
+        Iterator<String> iterator = list.iterator();//получение итератора для списка
+
+        while (iterator.hasNext())      //проверка, есть ли ещё элементы
+        {
+            //получение текущего элемента и переход на следующий
+            String text = iterator.next();
+
+            System.out.println(text);
+        }
+    }
+}
