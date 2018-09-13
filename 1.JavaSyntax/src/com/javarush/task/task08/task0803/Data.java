43a82e4a8ebcef921fdab9e4a220a5c730fea40c
Игорь <shilov0209@gmail.com>
2018-09-13 16:07:09 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0803/Data.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task08.task0803;
+
+import java.util.Date;
+
+public class Data {
+    public static void main(String[] args) {
+        Date today = new Date();
+        System.out.println("Current date : " + today);
+    }
+}
