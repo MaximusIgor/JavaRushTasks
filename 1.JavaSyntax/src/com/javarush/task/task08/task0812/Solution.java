ebc1ca3b75548ec065132cf3f81fd7f06f4153b1
Игорь <shilov0209@gmail.com>
2018-09-19 03:16:35 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task08.task0812;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Cамая длинная последовательность
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        ArrayList<Integer> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+    }
+}
