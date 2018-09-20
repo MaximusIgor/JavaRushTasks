2190d805411a35d01f47cfddec811890cd7a4be5
Игорь <shilov0209@gmail.com>
2018-09-21 00:56:03 +0300

    Solved.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+        Set<String> list = new HashSet<>();
+        list.add("Лес");
+        list.add("Лёд");
+        list.add("Лось");
+        list.add("Лис");
+        list.add("Лист");
+        list.add("Лак");
+        list.add("Лоза");
+        list.add("Листва");
+        list.add("Лук");
+        list.add("Лучь");
+        list.add("Лыжи");
+        list.add("Лай");
+        list.add("Лосось");
+        list.add("Ленейки");
+        list.add("Лампочка");
+        list.add("Леска");
+        list.add("Люстра");
+        list.add("Локомотив");
+        list.add("Лето");
+        list.add("Лужа");
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
